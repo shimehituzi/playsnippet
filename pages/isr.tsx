02c583450e3bc7e72b3b616f17953f78d0294ecf
shimehituzi <shimehituzi@gmail.com>
2021-09-15 18:34:09 +0900

    check isr

--- pages/isr.tsx
+++ pages/isr.tsx
@@ -2,17 +2,21 @@ import React from 'react'
 import { NextPage, GetStaticProps } from 'next'
 
 type Props = {
-  timestamp: string
+  date: string
+  timestamp: number
 }
 
 const ISR: NextPage<Props> = (props) => {
-  const csrTime = new Date().toLocaleString()
+  const csrDate = new Date().toLocaleString("ja-JP", { timeZone: "Asia/Tokyo" })
+  const csrTime = new Date().getTime()
+  const diff = csrTime - props.timestamp
 
   return (
     <React.Fragment>
       <h1>ISR ページ</h1>
-      <p>CSR time: {csrTime}</p>
-      <p>ISR time: {props.timestamp}</p>
+      <p>CSR date: {csrDate}</p>
+      <p>ISR date: {props.date}</p>
+      <p>diff: {diff}</p>
     </React.Fragment>
   )
 }
@@ -20,7 +24,8 @@ const ISR: NextPage<Props> = (props) => {
 export const getStaticProps: GetStaticProps<Props> = async () => {
   return {
     props: {
-      timestamp: new Date().toLocaleString(),
+      timestamp: new Date().getTime(),
+      date: new Date().toLocaleString("ja-JP", { timeZone: "Asia/Tokyo" }),
     },
     revalidate: 5,
   }
