36c79f51687a897db8448c96ad476adf0882b545
shimehituzi <shimehituzi@gmail.com>
2021-09-16 13:17:12 +0900

    Material-UI を使えるようにした

---
+++ pages/_app.tsx
@@ -0,0 +1,31 @@
+import React, { useEffect } from 'react'
+import { AppProps } from 'next/app'
+import Head from 'next/head'
+import { CssBaseline } from '@material-ui/core'
+import { ThemeProvider } from '@material-ui/core'
+import { theme } from '../src/theme'
+
+const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
+  useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side')
+    jssStyles?.parentElement?.removeChild(jssStyles)
+  })
+
+  return (
+    <React.Fragment>
+      <Head>
+        <title>Play Snippet</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline/>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </React.Fragment>
+  )
+}
+
+export default MyApp
---
+++ src/theme.ts
@@ -0,0 +1,10 @@
+import { colors } from '@material-ui/core'
+import { createTheme } from '@material-ui/core/styles'
+
+export const theme = createTheme({
+  palette: {
+    primary: colors.lightBlue,
+    secondary: colors.yellow,
+    type: 'dark',
+  },
+})
