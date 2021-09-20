99ba69d2e63ac9d584e7e11fd3bdb9b31f5c362c
shimehituzi <shimehituzi@gmail.com>
2021-09-20 15:51:58 +0900

    Create Post

--- pages/posts.tsx
+++ pages/posts.tsx
@@ -1,16 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NextPage } from 'next'
 import { AmplifySignOut } from '@aws-amplify/ui-react'
 import { useAuth } from '../src/hooks'
+import { API, graphqlOperation } from 'aws-amplify'
+import { createPost } from '../src/graphql/mutations'
 
 const Posts: NextPage = () => {
   const { authenticated } = useAuth()
 
   return (
     <React.Fragment>
-      { authenticated && <AmplifySignOut/> }
+      {authenticated && <AmplifySignOut />}
+      {authenticated && <Form />}
       <p>post page</p>
     </React.Fragment>
+  )
+}
+
+const Form: React.FC = () => {
+  const [content, setContent] = useState('')
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setContent(e.target.value)
+  }
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    const res = await API.graphql(
+      graphqlOperation(createPost, {
+        input: {
+          type: 'post',
+          content: content,
+        },
+      })
+    )
+
+    // eslint-disable-next-line no-console
+    console.log(res)
+
+    setContent('')
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" value={content} onChange={onChange} />
+      <button type="submit">create post</button>
+    </form>
   )
 }
 
