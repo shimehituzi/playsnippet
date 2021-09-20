a7877b73e8776bac43a626ec23a8b263c2217b4a
shimehituzi <shimehituzi@gmail.com>
2021-09-20 17:46:22 +0900

    Post の list と subscription をできるようにした

--- pages/posts.tsx
+++ pages/posts.tsx
@@ -1,19 +1,101 @@
-import React, { useState } from 'react'
+import React, { useEffect, useReducer, useState } from 'react'
 import { NextPage } from 'next'
 import { AmplifySignOut } from '@aws-amplify/ui-react'
 import { useAuth } from '../src/hooks'
-import { API, graphqlOperation } from 'aws-amplify'
+import API, { graphqlOperation, GraphQLResult } from '@aws-amplify/api'
 import { createPost } from '../src/graphql/mutations'
-import { CreatePostInput } from '../src/API'
+import {
+  CreatePostInput,
+  ListPostsByDateQueryVariables,
+  ModelSortDirection,
+  ListPostsByDateQuery,
+  OnCreatePostSubscription,
+  Post,
+} from '../src/API'
+import { listPostsByDate } from '../src/graphql/queries'
+import { onCreatePost } from '../src/graphql/subscriptions'
+import { Observable } from './../node_modules/zen-observable-ts'
+
+enum ActionType {
+  Subscription = 'subscription',
+  InitialQuery = 'initialQuery',
+  AdditionalQuery = 'additionalQuery',
+}
+
+type ReducerAction = {
+  type: ActionType
+  post?: Post
+  posts?: Post[]
+}
+
+const reducer = (state: Post[], action: ReducerAction) => {
+  switch (action.type) {
+    case ActionType.InitialQuery:
+      return action.posts
+    case ActionType.AdditionalQuery:
+      return [...state, ...action.posts]
+    case ActionType.Subscription:
+      return [action.post, ...state]
+    default:
+      return state
+  }
+}
 
 const Posts: NextPage = () => {
   const { authenticated } = useAuth()
 
+  const [posts, dispatch] = useReducer(reducer, [])
+  const [nextToken, setNextToken] = useState<string | null>(null)
+
+  const getPosts = async (type: ActionType, nextToken = null) => {
+    const queryInput: ListPostsByDateQueryVariables = {
+      type: 'post',
+      sortDirection: ModelSortDirection.DESC,
+      limit: 20,
+      nextToken: nextToken,
+    }
+
+    const res = (await API.graphql(
+      graphqlOperation(listPostsByDate, queryInput)
+    )) as GraphQLResult<ListPostsByDateQuery>
+
+    dispatch({ type: type, posts: res.data.listPostsByDate.items })
+    setNextToken(res.data.listPostsByDate.nextToken)
+  }
+
+  const getAdditionalPosts = () => {
+    if (nextToken === null) return
+    getPosts(ActionType.AdditionalQuery, nextToken)
+  }
+
+  useEffect(() => {
+    getPosts(ActionType.InitialQuery)
+
+    type Clinet = Observable<{ value: GraphQLResult<OnCreatePostSubscription> }>
+    const client = API.graphql(graphqlOperation(onCreatePost)) as Clinet
+    const subscription = client.subscribe({
+      next: (msg) => {
+        const post = msg.value.data.onCreatePost
+        dispatch({ type: ActionType.Subscription, post: post })
+      },
+    })
+
+    return () => {
+      subscription.unsubscribe()
+    }
+  }, [])
+
   return (
     <React.Fragment>
       {authenticated && <AmplifySignOut />}
+      <h1>post page</h1>
       {authenticated && <Form />}
-      <p>post page</p>
+      <li>
+        {posts.map((post, key) => (
+          <ul key={key}>{post.content}</ul>
+        ))}
+      </li>
+      <button onClick={getAdditionalPosts}>Read more</button>
     </React.Fragment>
   )
 }
