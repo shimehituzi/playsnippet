7e28f75dfc5eb6042a8cef2dd299e45ec239f0ed
shimehituzi <shimehituzi@gmail.com>
2021-09-23 17:17:12 +0900

    Delete feature

--- src/component/PostListItem.tsx
+++ src/component/PostListItem.tsx
@@ -3,15 +3,29 @@ import { Post } from '../API'
 import ReactMarkdown from 'react-markdown'
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
 import { vscDarkPlus } from 'react-syntax-highlighter/dist/esm/styles/prism'
-import { Typography } from '@material-ui/core'
+import { IconButton, Typography } from '@material-ui/core'
+import { Delete as DeleteIcon } from '@material-ui/icons'
 
 type Props = {
   post: Post
+  isOwner: boolean
+  handleDeletePost: (postId: string) => Promise<void>
 }
 
-export const PostListItem: React.FC<Props> = ({ post }) => {
+export const PostListItem: React.FC<Props> = ({
+  handleDeletePost,
+  post,
+  isOwner,
+}) => {
+  const onDelete = () => handleDeletePost(post.id)
+
   return (
     <React.Fragment>
+      {isOwner && (
+        <IconButton onClick={onDelete}>
+          <DeleteIcon />
+        </IconButton>
+      )}
       <ReactMarkdown>{post.content}</ReactMarkdown>
       {post.codes.items &&
         post.codes.items.map((code, key) => (
