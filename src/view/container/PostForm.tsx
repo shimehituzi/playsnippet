2bed0e283f63844f6be4b7873dbd9f3c6af69f9a
shimehituzi <shimehituzi@gmail.com>
2021-09-27 03:18:50 +0900

    code を複数投稿できる form を作成

--- src/view/container/PostForm.tsx
+++ src/view/container/PostForm.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useRecoilState } from 'recoil'
 import API, { graphqlOperation, GraphQLResult } from '@aws-amplify/api'
 import { createCode, createPost } from '../../graphql/mutations'
@@ -7,13 +7,36 @@ import {
   CreatePostMutation,
   CreateCodeMutationVariables,
 } from '../../API'
-import { Button, Grid, makeStyles, TextField } from '@material-ui/core'
+import {
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  colors,
+  Grid,
+  IconButton,
+  makeStyles,
+  Tab,
+  Tabs,
+  TextField,
+} from '@material-ui/core'
 import { postFormState } from '../../state/postFormState'
 import { CodeForm, codesFormState } from '../../state/codesFormState'
+import { Add as AddIcon, Close as CloseIcon } from '@material-ui/icons'
 
 const useStyle = makeStyles({
   form: {
     margin: '2%',
+  },
+  tab: {
+    textTransform: 'none',
+  },
+  card: {
+    backgroundColor: colors.grey[900],
+  },
+  close: {
+    marginLeft: 'auto',
   },
 })
 
@@ -22,6 +45,7 @@ export const PostForm: React.FC = () => {
 
   const [post, setPost] = useRecoilState(postFormState)
   const [codes, setCodes] = useRecoilState(codesFormState)
+  const [tab, setTab] = useState<number>(0)
 
   const handleSubmit = async () => {
     const createPostMutationVariables: CreatePostMutationVariables = {
@@ -75,8 +99,28 @@ export const PostForm: React.FC = () => {
       })
     }
 
+  const handleTabChange = (_e: React.SyntheticEvent, newTab: number) => {
+    setTab(newTab)
+  }
+
+  const addCode = () => {
+    const emptyItem: CodeForm = {
+      title: '',
+      content: '',
+      lang: '',
+    }
+    setCodes((codes) => [...codes, emptyItem])
+  }
+
+  const removeCode = (index: number) => () => {
+    if (window.confirm('Are you sure you want to delete code?')) {
+      setTab(0)
+      setCodes((codes) => [...codes.slice(0, index), ...codes.slice(index + 1)])
+    }
+  }
+
   return (
-    <Grid container justifyContent="center" alignItems="center">
+    <Grid container justifyContent="center" alignItems="center" spacing={4}>
       <Grid item xs={5} className={classes.form}>
         <TextField
           fullWidth
@@ -98,6 +142,79 @@ export const PostForm: React.FC = () => {
           name="content"
         />
       </Grid>
+      {codes.length > 0 ? (
+        <Grid item xs={12}>
+          <Box sx={{ width: '100%' }}>
+            <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+              <Tabs
+                value={tab}
+                onChange={handleTabChange}
+                variant="scrollable"
+                scrollButtons="auto"
+              >
+                {codes.map((code, index) => {
+                  const label =
+                    code.title === '' ? `Code ${index + 1}` : code.title
+                  return (
+                    <Tab key={index} label={label} className={classes.tab} />
+                  )
+                })}
+                <IconButton onClick={addCode}>
+                  <AddIcon />
+                </IconButton>
+              </Tabs>
+            </Box>
+            <Card className={classes.card}>
+              <CardActions>
+                <IconButton onClick={removeCode(tab)} className={classes.close}>
+                  <CloseIcon />
+                </IconButton>
+              </CardActions>
+              <CardContent>
+                <Grid container justifyContent="center" alignItems="center">
+                  <Grid item xs={5} className={classes.form}>
+                    <TextField
+                      fullWidth
+                      variant="standard"
+                      label="Code Title"
+                      value={codes[tab].title}
+                      onChange={onChangeCode(tab)}
+                      name="title"
+                    />
+                  </Grid>
+                  <Grid item xs={3} className={classes.form}>
+                    <TextField
+                      fullWidth
+                      variant="standard"
+                      label="Code Language"
+                      value={codes[tab].lang}
+                      onChange={onChangeCode(tab)}
+                      name="lang"
+                    />
+                  </Grid>
+                  <Grid item xs={12} className={classes.form}>
+                    <TextField
+                      fullWidth
+                      variant="outlined"
+                      label="Code"
+                      multiline
+                      value={codes[tab].content}
+                      onChange={onChangeCode(tab)}
+                      name="content"
+                    />
+                  </Grid>
+                </Grid>
+              </CardContent>
+            </Card>
+          </Box>
+        </Grid>
+      ) : (
+        <Grid item>
+          <Button variant="outlined" onClick={addCode}>
+            Add Code
+          </Button>
+        </Grid>
+      )}
       <Grid item>
         <Button variant="contained" onClick={handleSubmit}>
           create post
