f5ae54eed91e88241519eed8bf34d30d2044a29f
shimehituzi <shimehituzi@gmail.com>
2021-10-27 06:17:07 +0900

    api の State の Selector を実装

--- src/state/codesState.ts
+++
@@ -1,9 +0,0 @@
-import { atom } from 'recoil'
-import { Code } from '../API'
-
-export type OmittedCode = Omit<Code, 'post'>
-
-export const codesState = atom<OmittedCode[]>({
-  key: 'codesAtom',
-  default: [],
-})
--- src/state/commentsState.ts
+++
@@ -1,9 +0,0 @@
-import { atom } from 'recoil'
-import { Comment } from '../API'
-
-export type OmittedComment = Omit<Comment, 'post'>
-
-export const commentsState = atom<OmittedComment[]>({
-  key: 'commentsAtom',
-  default: [],
-})
